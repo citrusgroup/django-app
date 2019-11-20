d3a2ca15c876038fb91aa2738ce994c5bfeeb30f
Erik Boström <erikbostrom1@gmail.com>
2019-11-20 18:18:28 +0100

    add model properties

--- propertymatching/models.py
+++ propertymatching/models.py
@@ -1,8 +1,9 @@
-from django.db import models
+import datetime
+
 from django.contrib.postgres.fields import JSONField
+from django.db import models
 
-# Create your models here.
-
+# Partner models
 class Company(models.Model):
     name = models.CharField(max_length=50)
     phone_number = models.TextField(max_length=20, null=True)
@@ -24,5 +25,21 @@ class ListingItem(models.Model):
     monthly_cost = models.TextField(max_length=20)
     agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True)
     properties = JSONField()
+
+# User models
+class User(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class UserForm(models.Model):
+    budget_min = models.FloatField(null=True)
+    budget_max = models.FloatField(null=True)
+    monthly_cost_min = models.FloatField(null=True)
+    monthly_cost_max = models.FloatField(null=True)
+    properties = JSONField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
