1a4c9bc2e30401f1c71a4f3ce18d722de073df59
nathan-gerhardt <nathan_gerhardt@hotmail.com>
2019-11-28 23:14:31 +0100

    population tool expanded for customer form

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Install docker and docker-compose on your computer
 
 Run ``` docker-compose up --build ``` from the root directory.
 You should now have a postgres database on port 6432 and a django web application on port 8000
+
+
+### Population tool
+
+In the terminal, Run ``` docker-compose exec web python manage.py shell ```from the terminal after
+running ``` docker-compose up --build ```.
+Then in the terminal run the following:
+---------
+from propertymatching import population_tool
+population_tool.pop_randomiser.main()
+---------
+
+if you want to run the individual functions within population_tool, add the extension, e.g. the following:
+
+population_tool.insert_function_name()
--- propertymatching/population_tool.py
+++ propertymatching/population_tool.py
@@ -1,16 +1,22 @@
-from propertymatching.models import Company, Agent, ListingItem
+from propertymatching.models import Company, Agent, ListingItem, User, UserForm
 from faker import Faker
+import random
 
 
 def main():
     
     random_data = pop_randomiser()
 
-    populate_partnerform()(random_data)
+    populate_partnerform(random_data[0])
+
+    populate_customerform(random_data[1])
+
+    print('database populated!')
 
 def pop_randomiser():
 
     data = {}
+    data_userform = {}
     fake = Faker()
 
     fake.random.seed(5467)
@@ -44,7 +50,23 @@ def pop_randomiser():
                     'properties': fake.words(nb=15, ext_word_list=None, unique=False)
                 }
 
-    return data
+    fake.random.seed(5467)
+
+    for i in range(20):
+
+        user_name = fake.name()
+        budget = fake.ean8()
+        upkeep = fake.postalcode()
+        data_userform[user_name] = {
+            'user_number': fake.phone_number(),
+            'user_email': fake.free_email(),
+            'budget_min': (float(budget)*0.8),
+            'budget_max': (float(budget)*1.2),
+            'upkeep_min': (float(upkeep)*0.8),
+            'upkeep_max': (float(upkeep)*1.2),
+            'properties': fake.words(nb=15, ext_word_list=None, unique=False)
+        }
+    return data, data_userform
 
 def populate_partnerform(data):
 
@@ -70,9 +92,27 @@ def populate_partnerform(data):
 
                 listing_id.save()
     
-    
+    return
+def populate_customerform(cust_data):
 
-    return 'Partner Form populated!'
+    fake = Faker()
+    fake.random.seed(1000)
+    
+    for cust_name in cust_data:
+        customer_name = fake.name()
+
+        user_id = User(name=customer_name, phone_number=cust_data[cust_name]['user_number'],
+        email=cust_data[cust_name]['user_email'])
+
+        user_id.save()
+
+        userform_id = UserForm(budget_min=cust_data[cust_name]['budget_min'], budget_max=cust_data[cust_name]['budget_max'],
+        monthly_cost_min=cust_data[cust_name]['upkeep_min'], monthly_cost_max=cust_data[cust_name]['upkeep_max'],
+        properties=cust_data[cust_name]['properties'], user=user_id)
+
+        userform_id.save()
+
+    return
 
 if __name__ == "__main__":
     main()
