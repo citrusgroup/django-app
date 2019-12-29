a78193beab43705db2cf8d13a7587a93f8854a2c
nathan-gerhardt <nathan_gerhardt@hotmail.com>
2019-12-29 13:31:16 +0100

    search function finished, ready for review

--- propertymatching/search_function.py
+++ propertymatching/search_function.py
@@ -1,24 +1,22 @@
 from django.test import TestCase
 from propertymatching.models import Company, Agent, ListingItem, User, UserForm
 from django.db import connection
-from operator import eq
 
-# Create your tests here.
+
 def main():
+    # link up to the rest of the framework be executed every X hours
+    data_dict = search()
 
-    data_dict = search_test()
+    results = matches(data_dict)
 
-    #for i in data_dict['listing_results']:
-        #for j in data_dict['listing_results'][i]['listing_envs']:
-            #print(data_dict['listing_results'][i]['listing_envs'][j])
-    matches(data_dict)
+    results2model(results)
 
-def search_test():
+def search():
 
     query = queries()
 
     with connection.cursor() as cursor:
-        cursor.execute(query[1])
+        cursor.execute(query[2])
         data = cursor.fetchall()
 
     data_dict = {'listing_results': {}, 'user_results': {}}
@@ -32,18 +30,22 @@ def search_test():
             'listing_month_cost': row[3],
             'listing_properties': row[4],
             'listing_area': [row[5]],
-            'listing_envs': string_fixer(row[6])
+            'listing_envs': string_fixer(row[6]),
+            'listing_estate_type': [row[7]],
+            'listing_apart_level': [row[8]]
         }
 
         data_dict['user_results'][matching_no] = {
-            'user_id': row[7],
-            'user_min_budget': row[8],
-            'user_max_budget': row[9],
-            'user_min_month_cost': row[10],
-            'user_max_month_cost': row[11],
-            'user_properties': row[12],
-            'user_areas': string_fixer(row[13]),
-            'user_envs': string_fixer(row[14])
+            'user_id': row[9],
+            'user_min_budget': row[10],
+            'user_max_budget': row[11],
+            'user_min_month_cost': row[12],
+            'user_max_month_cost': row[13],
+            'user_properties': row[14],
+            'user_areas': string_fixer(row[15]),
+            'user_envs': string_fixer(row[16]),
+            'user_estate_type': string_fixer(row[17]),
+            'user_apart_level': string_fixer(row[18])
         }
         matching_no +=1    
 
@@ -60,24 +62,38 @@ def matches(data_dict):
         env_listing = data_dict['listing_results'][entry]['listing_envs']
         env_user = data_dict['user_results'][entry]['user_envs']
 
+        listing_estate = data_dict['listing_results'][entry]['listing_estate_type']
+        user_estate = data_dict['user_results'][entry]['user_estate_type']
+
         if any(elm in user_areas for elm in area_listing):
+            if any(e_type in user_estate for e_type in listing_estate):
+                user_envs = len(env_user)
+                listing_envs = len(env_listing)
 
-            user_envs = len(env_user)
-            listing_envs = len(env_listing)
-
-            env_matching = 0
-            for i in env_user:
-                for j in env_listing:
-                    if i==j:
-                        env_matching +=1
-            if (env_matching/user_envs)*100>=80:
-                print(data_dict['listing_results'][entry])
-                print(data_dict['user_results'][entry])
-
-                matches += 1
+                env_matching = 0
+                for i in env_user:
+                    for j in env_listing:
+                        if i==j:
+                            env_matching +=1
+                if (env_matching/user_envs)*100>=80:
+                    matching_dict[matches] = {
+                        'listing_id': data_dict['listing_results'][entry]['listing_id'],
+                        'user_id': data_dict['user_results'][entry]['user_id']
+                    }
+                    print('match found!')
+                    print('listing_id: ', data_dict['listing_results'][entry]['listing_id'])
+                    print('user_id: ', data_dict['user_results'][entry]['user_id'])
+                    matches += 1
 
     print('total matches: ', matches)
-    return 
+    return matching_dict
+def results2model(search_results):
+
+
+
+
+    return
+
 
 def queries():
 
@@ -134,11 +150,43 @@ def queries():
             AND user_details.properties -> 'garden' = listings.properties -> 'garden'
             AND user_details.properties -> 'balcony' = listings.properties -> 'balcony'
             AND user_details.properties -> 'rent_out' = listings.properties -> 'rent_out'
-            
         ORDER BY
             user_id"""
-
-    return search_light, search_medium
+    search_heavy ="""SELECT
+	--property listings results
+	listings.id as listing_id,
+	listings.name as listing_name,
+	listings.price as listing_price,
+	listings.monthly_cost as listing_month_cost,
+	listings.properties as listing_properties,
+	listings.properties ->> 'area' as listing_area,
+	listings.properties ->> 'environment' as listing_envs,
+	listings.properties ->> 'real_estate_type' as listing_estate_type,
+	listings.properties ->> 'apartment_level' as listing_apart_level,
+	--user requirements results
+	user_details.id as user_id,
+	user_details.budget_min as user_min_budget,
+	user_details.budget_max as user_max_budget,
+	user_details.monthly_cost_min as user_min_month_cost,
+	user_details.monthly_cost_max as user_max_month_cost,
+	user_details.properties as user_properties,
+	user_details.properties ->> 'area' as user_areas,
+	user_details.properties ->> 'environment' as user_envs,
+	user_details.properties ->> 'real_estate_type' as user_estate_type,
+	user_details.properties ->> 'apartment_level' as user_apart_level
+    FROM
+	propertymatching_userform AS user_details
+	JOIN propertymatching_listingitem AS listings ON user_details.budget_max >= listings.price
+		AND user_details.budget_min <= listings.price
+		AND listings.monthly_cost BETWEEN user_details.monthly_cost_min AND user_details.monthly_cost_max
+		AND user_details.properties -> 'pool' = listings.properties -> 'pool'
+		AND user_details.properties -> 'garden' = listings.properties -> 'garden'
+		AND user_details.properties -> 'balcony' = listings.properties -> 'balcony'
+		AND user_details.properties -> 'rent_out' = listings.properties -> 'rent_out'
+		AND listings.properties -> 'size' BETWEEN user_details.properties -> 'size_min' AND user_details.properties -> 'size_max'
+    ORDER BY
+	user_id"""
+    return search_light, search_medium, search_heavy
 
 def string_fixer(entity):
     rem_01 = entity.strip('[]')
@@ -146,5 +194,7 @@ def string_fixer(entity):
     rem_03 = rem_02.replace(' ', '')
     rem_04 = rem_03.split(',')
     return rem_04
+
+
 if __name__== "__main__":
     main()
