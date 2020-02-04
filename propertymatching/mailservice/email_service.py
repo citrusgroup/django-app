88f35a935e89e3da98277117445e12f0ca0f9d31
Nathan Gerhardt <nathangerhardt1992@gmail.com>
2020-02-04 13:35:42 +0100

    perf(mailservice): sendgrid compatible
    created the mail service for sendgrid API which can now send out a basic email
    
    PR Closed #1001

--- propertymatching/mailservice/email_service.py
+++ propertymatching/mailservice/email_service.py
@@ -1,20 +1,30 @@
-from django.http import HttpResponse, JsonResponse
-from propertymatching.models import Company, Agent, ListingItem, User, UserForm, FormMatches
-from django.db import connection
-from .utils import SendMatchEmail
-from propertymatching.mailservice import source
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from form_source import email_source
 
-def searchmatch(request):
-    if request.method =='POST':
-        email_reqs = source.email_source()
-        for match_found in email_reqs:
-            try:
-                email = email_reqs[match_found]['customer']['email']
-                SendMatchEmail(email)
+def mail_exec():
 
-            except:
-                callback = {"status - bad request", "400"}
-                return JsonResponse(callback)
+    match_data = email_source()
     
-    return HttpResponse("/")
+    
+    emails = ['John.holmgren@tmrw.se', 'erikbostrom1@gmail.com', 'magnus.larsen@apoex.se']
+
+    for mail in emails:
+        message = Mail(
+            from_email = 'sendgrid_email_test@example.com',
+            to_emails =mail,
+            subject= 'Hello from the Citrus Group Automated email service - please take a ticket',
+            html_content='<strong>locked and loaded, watch this space and get excited</strong>')
+        print(os.environ.get('SENDGRID_API_KEY'))
+        try:
+            sg = SendGridAPIClient(api_key='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')
+
+            response = sg.send(message)
+            print(mail)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(str(e))
 
