13b9e24660c79bf5824c4bfb449aa6b311f846bc
Nathan Gerhardt <nathangerhardt1992@gmail.com>
2020-02-05 13:20:04 +0100

    feat(mailservice): retrieved source data
    wrote temp function to retrieve source data from db
    
    Issue #MS2005

--- propertymatching/mailservice/email_service.py
+++ propertymatching/mailservice/email_service.py
@@ -1,13 +1,12 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-from form_source import email_source
+from propertymatching.mailservice import form_source
 
 def mail_exec():
 
-    match_data = email_source()
-    
-    
+
+
     emails = ['John.holmgren@tmrw.se', 'erikbostrom1@gmail.com', 'magnus.larsen@apoex.se']
 
     for mail in emails:
@@ -28,3 +27,35 @@ def mail_exec():
         except Exception as e:
             print(str(e))
 
+
+def source_mail():
+    
+    match_data = form_source.email_source()
+
+    for match_no in match_data:
+        
+        #data on the contact details of the customer
+        print(match_no)
+        print(match_data[match_no]['customer']['name'])
+        print(match_data[match_no]['customer']['phone'])
+        print(match_data[match_no]['customer']['email'])
+
+        #data on the listing of the match with aformentioned customer
+        print(match_data[match_no]['listing']['listing_address'])
+        print(match_data[match_no]['listing']['listing_price'])
+        print(match_data[match_no]['listing']['upkeep'])
+        print(match_data[match_no]['listing']['details'])
+        #print(match_data[match_no]['listing']['environment'])
+        #print(match_data[match_no]['listing']['apartment_level'])
+        #print(match_data[match_no]['listing']['real_estate_type'])
+
+        #data on the agent details of the match with aformentioned customer
+        print(match_data[match_no]['agent']['agent_name'])
+        print(match_data[match_no]['agent']['phone'])
+        print(match_data[match_no]['agent']['email'])
+        
+        #data on the compay details of the match with aformentioned customer
+        print(match_data[match_no]['company']['company'])
+        print(match_data[match_no]['company']['phone'])
+        print(match_data[match_no]['company']['email'])
+        print('\n')
